34cd34c5b1ad03ec31a490648c02d936a7e5eeab
fborch <fborch@gmail.com>
2026-04-19 08:52:38 +0200

    Auto-backup WoW settings: 2026-04-19_08-52-37

---
+++ 2026-04-19_08-52-37/RaiderIO.lua
@@ -0,0 +1,31 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Bandwagoner-draenor"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Mortixia-draenor"] = true,
+["eu-Lauralefur-draenor"] = true,
+["eu-Темник-deathguard"] = true,
+["eu-Rhuuark-stormrage"] = true,
+["eu-Sunsue-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
