8eb32ad97955e9d06647ec9a4ee3c2639b5eaeaf
fborch <fborch@gmail.com>
2026-04-19 15:20:31 +0200

    Auto-backup WoW settings: 2026-04-19_15-20-30

---
+++ 2026-04-19_15-20-30/WorldQuestTab.lua
@@ -0,0 +1,35 @@
+
+BWQDB = {
+["char"] = {
+["Cooriam - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Abaray - Draenor"] = {
+["activeProfile"] = 0,
+},
+},
+["global"] = {
+["versionCheck"] = 120010,
+["profiles"] = {
+[0] = {
+["colors"] = {
+},
+["name"] = "Default",
+["list"] = {
+},
+["general"] = {
+},
+["external"] = {
+},
+["pin"] = {
+},
+["filters"] = {
+},
+},
+},
+},
+["profileKeys"] = {
+["Cooriam - Draenor"] = "Default",
+["Abaray - Draenor"] = "Default",
+},
+}
