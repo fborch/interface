991586e1a14db66bd9e64e7fabf24c09894b33ef
fborch <fborch@gmail.com>
2026-04-20 19:58:34 +0200

    Auto-backup WoW settings: 2026-04-20_19-58-33

---
+++ 2026-04-20_19-58-33/MyusKnowledgePointsTracker.lua.bak
@@ -0,0 +1,21 @@
+
+MKPT_Config = {
+["char"] = {
+["Cooriam - Draenor"] = {
+["state"] = {
+["show"] = false,
+},
+},
+},
+["profileKeys"] = {
+["Sedore - Draenor"] = "Sedore - Draenor",
+["Gembank - Stormrage"] = "Gembank - Stormrage",
+["Corwing - Draenor"] = "Corwing - Draenor",
+["Coorlan - Draenor"] = "Coorlan - Draenor",
+["Abaray - Draenor"] = "Abaray - Draenor",
+["Corra - Draenor"] = "Corra - Draenor",
+["Unlockit - Draenor"] = "Unlockit - Draenor",
+["Cooriam - Draenor"] = "Cooriam - Draenor",
+["Decoran - Draenor"] = "Decoran - Draenor",
+},
+}
---
+++ 2026-04-20_19-58-33/WorldQuestTab.lua
@@ -0,0 +1,55 @@
+
+BWQDB = {
+["char"] = {
+["Sedore - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corra - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Unlockit - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Decoran - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Coorlan - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Cooriam - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Abaray - Draenor"] = {
+["activeProfile"] = 0,
+},
+},
+["global"] = {
+["versionCheck"] = 120010,
+["profiles"] = {
+[0] = {
+["colors"] = {
+},
+["name"] = "Default",
+["list"] = {
+},
+["general"] = {
+},
+["pin"] = {
+},
+["external"] = {
+},
+["filters"] = {
+},
+},
+},
+},
+["profileKeys"] = {
+["Sedore - Draenor"] = "Default",
+["Corra - Draenor"] = "Default",
+["Unlockit - Draenor"] = "Default",
+["Decoran - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Abaray - Draenor"] = "Default",
+},
+}
