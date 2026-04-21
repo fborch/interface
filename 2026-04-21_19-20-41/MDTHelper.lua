b2961320b11cb94944df6e7830c42a71f7f8e1c2
fborch <fborch@gmail.com>
2026-04-21 19:20:41 +0200

    Auto-backup WoW settings: 2026-04-21_19-20-41

---
+++ 2026-04-21_19-20-41/MDTHelper.lua
@@ -0,0 +1,31 @@
+
+MDTHelperDB = {
+["enabled"] = true,
+["mapPopoutSize"] = {
+299.9999694824219,
+537.75,
+},
+["autoAdvance"] = true,
+["bgAlpha"] = 1,
+["uiScale"] = 1,
+["mapFullRoute"] = false,
+["pullThreshold"] = 0.8999999761581421,
+["mapPopoutPoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+0,
+-275.9996948242188,
+},
+["minimized"] = true,
+["mapPopout"] = true,
+["autoImport"] = true,
+["locked"] = false,
+["frameHeight"] = 306.5000305175781,
+["framePoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+2.500002384185791,
+-2.00003719329834,
+},
+["mapShowSurround"] = true,
+}
---
+++ 2026-04-21_19-20-41/RaiderIO.lua
@@ -0,0 +1,25 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Decoran-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Decoran-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
