4f63f23c0ad6e6a7aa96b538279fb5be1b2e3cf6
fborch <fborch@gmail.com>
2026-04-21 23:22:43 +0200

    Auto-backup WoW settings: 2026-04-21_23-22-43

---
+++ 2026-04-21_23-22-43/MDTHelper.lua
@@ -0,0 +1,31 @@
+
+MDTHelperDB = {
+["enabled"] = true,
+["mapPopoutSize"] = {
+299.9999694824219,
+537.75,
+},
+["autoAdvance"] = true,
+["bgAlpha"] = 1,
+["frameHeight"] = 306.5000305175781,
+["pullThreshold"] = 0.8999999761581421,
+["mapPopout"] = true,
+["mapPopoutPoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+0,
+-275.9996948242188,
+},
+["mapFullRoute"] = false,
+["minimized"] = true,
+["autoImport"] = true,
+["locked"] = false,
+["framePoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+2.500002384185791,
+-2.00003719329834,
+},
+["uiScale"] = 1,
+["mapShowSurround"] = true,
+}
