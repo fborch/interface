7aa7cea0c8494d0ff6393bd3ed401df68fffcb65
fborch <fborch@gmail.com>
2026-04-22 09:55:22 +0200

    Auto-backup WoW settings: 2026-04-22_09-55-22

---
+++ 2026-04-22_09-55-22/RaiderIO.lua
@@ -0,0 +1,52 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Cooriam-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Guttzint-draenor"] = true,
+["eu-Бусиктцк-soulflayer"] = true,
+["eu-Amantulica-draenor"] = true,
+["eu-Pedróó-draenor"] = true,
+["eu-Lektorhunter-draenor"] = true,
+["eu-Enchrouge-draenor"] = true,
+["eu-Lyríá-draenor"] = true,
+["eu-Hisus-draenor"] = true,
+["eu-Thanagôr-draenor"] = true,
+["eu-Snugdin-draenor"] = true,
+["eu-Zimhra-draenor"] = true,
+["eu-Veltryl-draenor"] = true,
+["eu-Shortbreads-draenor"] = true,
+["eu-Indid-draenor"] = true,
+["eu-Navalge-draenor"] = true,
+["eu-Sasí-draenor"] = true,
+["eu-Mordium-draenor"] = true,
+["eu-Sypo-draenor"] = true,
+["eu-Zemmr-draenor"] = true,
+["eu-Voidant-draenor"] = true,
+["eu-Slyflow-draenor"] = true,
+["eu-Adoomz-draenor"] = true,
+["eu-Fariice-draenor"] = true,
+["eu-Hightide-draenor"] = true,
+["eu-Felclad-draenor"] = true,
+["eu-Jimoozie-draenor"] = true,
+["eu-Pwv-draenor"] = true,
+["eu-Thóràx-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
