5848219d3237ee442b81aa119dca405d6751ab5b
fborch <fborch@gmail.com>
2026-04-22 11:37:23 +0200

    Auto-backup WoW settings: 2026-04-22_11-37-23

---
+++ 2026-04-22_11-37-23/MDTHelper.lua
@@ -0,0 +1,31 @@
+
+MDTHelperDB = {
+["enabled"] = true,
+["mapPopoutSize"] = {
+299.9999694824219,
+537.75,
+},
+["autoAdvance"] = true,
+["bgAlpha"] = 1,
+["framePoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+2.500002384185791,
+-2.00003719329834,
+},
+["pullThreshold"] = 0.8999999761581421,
+["minimized"] = true,
+["mapPopoutPoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+0,
+-275.9996948242188,
+},
+["mapPopout"] = false,
+["mapFullRoute"] = false,
+["autoImport"] = true,
+["locked"] = false,
+["uiScale"] = 1,
+["frameHeight"] = 306.5000305175781,
+["mapShowSurround"] = true,
+}
---
+++ 2026-04-22_11-37-23/RaiderIO.lua
@@ -0,0 +1,28 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Japwank-draenor"] = true,
+["eu-Trappetrinn-draenor"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Bitemesoftly-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
