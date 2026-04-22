4cc864bcf279240d97b2b2c64c10e091b8778401
fborch <fborch@gmail.com>
2026-04-22 12:29:53 +0200

    Auto-backup WoW settings: 2026-04-22_12-29-53

---
+++ 2026-04-22_12-29-53/MDTHelper.lua
@@ -0,0 +1,31 @@
+
+MDTHelperDB = {
+["enabled"] = true,
+["mapPopoutSize"] = {
+299.9999694824219,
+537.75,
+},
+["autoAdvance"] = true,
+["bgAlpha"] = 1,
+["frameHeight"] = 306.5000305175781,
+["mapFullRoute"] = false,
+["mapPopout"] = false,
+["mapPopoutPoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+0,
+-275.9996948242188,
+},
+["pullThreshold"] = 0.8999999761581421,
+["minimized"] = true,
+["autoImport"] = true,
+["locked"] = false,
+["framePoint"] = {
+"TOPLEFT",
+"TOPLEFT",
+2.500002384185791,
+-2.00003719329834,
+},
+["uiScale"] = 1,
+["mapShowSurround"] = true,
+}
---
+++ 2026-04-22_12-29-53/RaiderIO.lua
@@ -0,0 +1,35 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Buddars-draenor"] = true,
+["eu-Calozz-draenor"] = true,
+["eu-Heliphas-draenor"] = true,
+["eu-Wushandang-draenor"] = true,
+["eu-Zohare-draenor"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Detstoresvin-draenor"] = true,
+["eu-Shnk-draenor"] = true,
+["eu-Kunin-draenor"] = true,
+["eu-Druweéd-draenor"] = true,
+["eu-Lücy-xavius"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
