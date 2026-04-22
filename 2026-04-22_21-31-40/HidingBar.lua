a19aff5baecd3e41f7c69992698ea3f10cfd11f0
fborch <fborch@gmail.com>
2026-04-22 21:31:40 +0200

    Auto-backup WoW settings: 2026-04-22_21-31-40

---
+++ 2026-04-22_21-31-40/HidingBar.lua
@@ -0,0 +1,139 @@
+
+HidingBarDB = {
+["tstmp"] = 1776613086,
+["profiles"] = {
+{
+["isDefault"] = true,
+["config"] = {
+["mbtnSettings"] = {
+["LibDBIcon10_Details"] = {
+["tstmp"] = 1776613087,
+},
+["LibDBIcon10_CraftSim"] = {
+["tstmp"] = 1775683057,
+},
+["LibDBIcon10_RaiderIO"] = {
+["tstmp"] = 1775683057,
+},
+["PlumberLandingPageMinimapButton"] = {
+["tstmp"] = 1776613086,
+},
+["LibDBIcon10_BigWigs"] = {
+["tstmp"] = 1775683057,
+},
+["LibDBIcon10_DandersFrames"] = {
+["tstmp"] = 1776613087,
+},
+["LibDBIcon10_BugSack"] = {
+["tstmp"] = 1775683057,
+},
+["FontMagicMinimapButton"] = {
+["tstmp"] = 1775683057,
+},
+["MinimapButton_D4Lib_LibDBIcon_MoveAny"] = {
+["tstmp"] = 1775680991,
+},
+["LibDBIcon10_DetailsStreamer"] = {
+["tstmp"] = 1776447078,
+},
+["LibDBIcon10_EnhanceQoL_ButtonSinkMap"] = {
+["tstmp"] = 1776613086,
+},
+["LibDBIcon10_ClassUIEnhanced"] = {
+["tstmp"] = 1775683057,
+},
+["LibDBIcon10_EnhanceQoL"] = {
+["tstmp"] = 1775683057,
+},
+},
+["btnSettings"] = {
+["HidingBar"] = {
+["tstmp"] = 1776613086,
+},
+},
+["grabMinimap"] = true,
+["grabMinimapAfterN"] = 1,
+["customGrabList"] = {
+},
+["ombGrabQueue"] = {
+},
+["ignoreMBtn"] = {
+},
+},
+["name"] = "Profile 1",
+["bars"] = {
+{
+["isDefault"] = true,
+["config"] = {
+["lineWidth"] = 4,
+["secondPosition"] = 0,
+["rangeBetweenBtns"] = 0,
+["lineBorderColor"] = {
+1,
+1,
+1,
+1,
+},
+["showDelay"] = 0,
+["bgTexture"] = "Solid",
+["borderColor"] = {
+1,
+1,
+1,
+1,
+},
+["anchor"] = "top",
+["lineTexture"] = "Solid",
+["barTypePosition"] = 0,
+["size"] = 10,
+["interceptTooltipPosition"] = 0,
+["petBattleHide"] = true,
+["lineColor"] = {
+0.8,
+0.6,
+0,
+},
+["position"] = 1691.308185081912,
+["mbtnPosition"] = 2,
+["lineBorderEdge"] = false,
+["lineBorderOffset"] = 1,
+["showHandler"] = 2,
+["expand"] = 1,
+["borderEdge"] = false,
+["bgColor"] = {
+0.1,
+0.1,
+0.1,
+0.7,
+},
+["hideDelay"] = 0.75,
+["gapSize"] = 0,
+["interceptTooltip"] = true,
+["buttonDirection"] = {
+["V"] = 0,
+["H"] = 0,
+},
+["borderOffset"] = 4,
+["omb"] = {
+["hide"] = true,
+["fadeOpacity"] = 1,
+["anchor"] = "right",
+["barDisplacement"] = 0,
+["size"] = 31,
+["distanceToBar"] = 0,
+},
+["buttonSize"] = 31,
+["frameStrata"] = 2,
+["lineBorderSize"] = 2,
+["fadeOpacity"] = 0.2,
+["orientation"] = 0,
+["barOffset"] = 2,
+["hideHandler"] = 2,
+["borderSize"] = 16,
+},
+["name"] = "Bar 1",
+},
+},
+},
+},
+}
