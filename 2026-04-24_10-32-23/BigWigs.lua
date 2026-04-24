6c1087016f8a1ec5b7e84e0a6d99923a008305d0
fborch <fborch@gmail.com>
2026-04-24 10:32:24 +0200

    Auto-backup WoW settings: 2026-04-24_10-32-23

---
+++ 2026-04-24_10-32-23/BigWigs.lua
@@ -0,0 +1,154 @@
+
+BigWigs3DB = {
+["profileKeys"] = {
+["Decoran - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+["Gembank - Stormrage"] = "Default",
+["Corwing - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Corzumo - Draenor"] = "Default",
+["Abaray - Draenor"] = "Default",
+["Nothree - Draenor"] = "Default",
+["Corzumo - Stormrage"] = "Default",
+["Corra - Draenor"] = "Default",
+["Unlockit - Draenor"] = "Default",
+["Corry - Draenor"] = "Default",
+["Sedore - Draenor"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Corpy - Draenor"] = "Default",
+},
+["namespaces"] = {
+["BigWigs_Plugins_PrivateAuras"] = {
+["global"] = {
+["showHelpTip"] = false,
+},
+},
+["BigWigs_Plugins_Timeline"] = {
+["profiles"] = {
+["Default"] = {
+["oneTimeTimelineDisable"] = true,
+},
+},
+},
+["BigWigs_Plugins_BossBlock"] = {
+["global"] = {
+["tableNeedsCopied"] = false,
+["watchedMovies"] = {
+[-2238] = {
+true,
+true,
+},
+[1003] = true,
+[-2233] = true,
+[-437] = true,
+},
+},
+},
+["BigWigs_Bosses_Nullaeus"] = {
+["profiles"] = {
+["Default"] = {
+["adds"] = 0,
+[1256518] = 0,
+[1280086] = 3064567,
+[1280087] = 0,
+[1280088] = 0,
+[1256045] = 0,
+[1256526] = 0,
+[1256167] = 0,
+[1256366] = 0,
+},
+},
+},
+["MythicPlus"] = {
+["profiles"] = {
+["Default"] = {
+["showViewerDungeonEnd"] = false,
+["progressTooltip"] = false,
+},
+},
+},
+},
+["myKeystones"] = {
+["Player-1403-0AF8D36C"] = {
+["playerRating"] = 678,
+["specId"] = 104,
+["keyMap"] = 559,
+["name"] = "Abaray",
+["keyLevel"] = 3,
+["realm"] = "Draenor",
+},
+["Player-1403-0ADB8461"] = {
+["playerRating"] = 0,
+["specId"] = 73,
+["keyMap"] = 0,
+["name"] = "Coorlan",
+["keyLevel"] = 0,
+["realm"] = "Draenor",
+},
+["Player-1403-0AD4DC55"] = {
+["playerRating"] = 3019,
+["specId"] = 268,
+["keyMap"] = 556,
+["name"] = "Cooriam",
+["keyLevel"] = 14,
+["realm"] = "Draenor",
+},
+["Player-1403-0AF8D385"] = {
+["playerRating"] = 0,
+["specId"] = 256,
+["keyMap"] = 0,
+["name"] = "Sedore",
+["keyLevel"] = 0,
+["realm"] = "Draenor",
+},
+["Player-1403-0AF6E911"] = {
+["playerRating"] = 0,
+["specId"] = 255,
+["keyMap"] = 0,
+["name"] = "Corra",
+["keyLevel"] = 0,
+["realm"] = "Draenor",
+},
+["Player-1403-0B41293A"] = {
+["playerRating"] = 0,
+["specId"] = 259,
+["keyMap"] = 0,
+["name"] = "Corzos",
+["keyLevel"] = 0,
+["realm"] = "Draenor",
+},
+["Player-1403-0B3FD2BA"] = {
+["playerRating"] = 0,
+["specId"] = 266,
+["keyMap"] = 0,
+["name"] = "Decoran",
+["keyLevel"] = 0,
+["realm"] = "Draenor",
+},
+},
+["prevWeeklyReset"] = 1776830400,
+["profiles"] = {
+["Default"] = {
+},
+},
+}
+BigWigsIconDB = {
+}
+BigWigsStatsDB = {
+[2966] = {
+[-3372] = {
+["solotier11"] = {
+["kills"] = 1,
+["fkDuration"] = 628.5110000000004,
+["wipes"] = 5,
+["fkWipes"] = 5,
+["best"] = 628.5110000000004,
+["bestDate"] = "2026/04/10",
+["fkDate"] = "2026/04/10",
+},
+},
+},
+}
+BW_FEAT_SHARE = nil
+BW_FEAT_SHARE2 = nil
+BW_FEAT_M_PERCENT = true
---
+++ 2026-04-24_10-32-23/RaiderIO.lua
@@ -0,0 +1,30 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Wealthdh-draenor"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Asceticbliss-draenor"] = true,
+["eu-Gisp-draenor"] = true,
+["eu-Uddersmoothi-draenor"] = true,
+["eu-Girejepisw-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
