2e495882db1f1991ea6690db07e10886ad86c8af
fborch <fborch@gmail.com>
2026-04-24 09:04:41 +0200

    Auto-backup WoW settings: 2026-04-24_09-04-41

---
+++ 2026-04-24_09-04-41/RaiderIO.lua
@@ -0,0 +1,38 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Cooriam-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Kallerdh-drakthul"] = true,
+["eu-party5-draenor"] = true,
+["eu-Bigmo-twisting-nether"] = true,
+["eu-Andérssón-draenor"] = true,
+["eu-Elxipal-stormreaver"] = true,
+["eu-Suntripdh-stormscale"] = true,
+["eu-Dariôn-twisting-nether"] = true,
+["eu-Shankos-uldum"] = true,
+["eu-Hardhunthord-burning-legion"] = true,
+["eu-Dangalfus-eredar"] = true,
+["eu-Grötlisa-outland"] = true,
+["eu-Jiztdru-ragnaros"] = true,
+["eu-Verkligen-kazzak"] = true,
+["eu-Zondun-tichondrius"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
