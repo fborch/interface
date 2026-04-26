eebcecac159f56e819396b9be64c610c914dc36a
fborch <fborch@gmail.com>
2026-04-26 21:54:19 +0200

    Auto-backup WoW settings: 2026-04-26_21-54-18

---
+++ 2026-04-26_21-54-18/FontMagic.lua
@@ -0,0 +1,20 @@
+
+FontMagicDB = {
+["minimapAngle"] = 225,
+["minimapHide"] = false,
+["combatOverrides"] = {
+},
+["floatingTextFadeDuration"] = 1,
+["extraCombatOverrides"] = {
+},
+["combatTextOutlineMode"] = "NONE",
+["__fmPopularFolderMigrationDone"] = true,
+["showExtraCombatToggles"] = false,
+["incomingOverrides"] = {
+},
+["__fmDebugCombatText"] = false,
+["favorites"] = {
+},
+["floatingTextGravity"] = 0.5,
+["selectedFont"] = "Popular/Roboto-Bold.ttf",
+}
---
+++ 2026-04-26_21-54-18/RaiderIO.lua
@@ -0,0 +1,25 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Corzos-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Corzos-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
