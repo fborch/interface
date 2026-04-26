724700b21acf26abb8fbe1d030d73410a8911419
fborch <fborch@gmail.com>
2026-04-26 19:25:52 +0200

    Auto-backup WoW settings: 2026-04-26_19-25-51

---
+++ 2026-04-26_19-25-51/WorldQuestTab.lua
@@ -0,0 +1,75 @@
+
+BWQDB = {
+["char"] = {
+["Decoran - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Nothree - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Coorlan - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Abaray - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Sedore - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzumo - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corra - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Unlockit - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corry - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzumo - Stormrage"] = {
+["activeProfile"] = 0,
+},
+["Cooriam - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzos - Draenor"] = {
+["activeProfile"] = 0,
+},
+},
+["global"] = {
+["versionCheck"] = 120011,
+["profiles"] = {
+[0] = {
+["colors"] = {
+},
+["name"] = "Default",
+["list"] = {
+},
+["general"] = {
+},
+["external"] = {
+},
+["pin"] = {
+},
+["filters"] = {
+},
+},
+},
+},
+["profileKeys"] = {
+["Decoran - Draenor"] = "Default",
+["Nothree - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Abaray - Draenor"] = "Default",
+["Sedore - Draenor"] = "Default",
+["Corzumo - Draenor"] = "Default",
+["Corra - Draenor"] = "Default",
+["Unlockit - Draenor"] = "Default",
+["Corry - Draenor"] = "Default",
+["Corzumo - Stormrage"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+},
+}
