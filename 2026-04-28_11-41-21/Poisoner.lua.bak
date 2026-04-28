08500de4d9a8b88d47ed9f304b81219407ee8176
fborch <fborch@gmail.com>
2026-04-28 11:41:22 +0200

    Auto-backup WoW settings: 2026-04-28_11-41-21

---
+++ 2026-04-28_11-41-21/Poisoner.lua.bak
@@ -0,0 +1,26 @@
+
+PoisonerDB = {
+["namespaces"] = {
+},
+["profileKeys"] = {
+["Cooriam - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+},
+["profiles"] = {
+["Default"] = {
+["poisons"] = {
+["lastused"] = {
+["Lethal"] = 2823,
+["Non-Lethal"] = 3408,
+},
+},
+["freebutton"] = {
+["point"] = {
+["y"] = 27.00015620624754,
+["rp"] = "BOTTOMLEFT",
+["x"] = 2334.000456091948,
+},
+},
+},
+},
+}
---
+++ 2026-04-28_11-41-21/RaiderIO.lua
@@ -0,0 +1,32 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Corzos-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Limenko-draenor"] = true,
+["eu-Cunel-draenor"] = true,
+["eu-Krvavihunt-draenor"] = true,
+["eu-Ioyal-draenor"] = true,
+["eu-Slÿngel-draenor"] = true,
+["eu-Corzos-draenor"] = true,
+["eu-Rèflékzx-draenor"] = true,
+["eu-Dualkegw-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
