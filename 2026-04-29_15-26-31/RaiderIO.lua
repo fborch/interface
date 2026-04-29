eec4ee478402434a113c6cdede62dcc3701ac937
fborch <fborch@gmail.com>
2026-04-29 15:26:31 +0200

    Auto-backup WoW settings: 2026-04-29_15-26-31

---
+++ 2026-04-29_15-26-31/RaiderIO.lua
@@ -0,0 +1,28 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Corzos-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Corzos-draenor"] = true,
+["eu-Miaoulechat-draenor"] = true,
+["eu-Meredy Huntswell-draenor"] = true,
+["eu-Coponeve-doomhammer"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
