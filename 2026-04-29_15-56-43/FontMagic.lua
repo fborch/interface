789729db3d64af72e372dc83fbd4ef95507f8291
fborch <fborch@gmail.com>
2026-04-29 15:56:44 +0200

    Auto-backup WoW settings: 2026-04-29_15-56-43

---
+++ 2026-04-29_15-56-43/FontMagic.lua
@@ -0,0 +1,20 @@
+
+FontMagicDB = {
+["minimapAngle"] = 225,
+["minimapHide"] = false,
+["combatOverrides"] = {
+},
+["floatingTextFadeDuration"] = 1,
+["extraCombatOverrides"] = {
+},
+["combatTextOutlineMode"] = "NONE",
+["__fmPopularFolderMigrationDone"] = true,
+["favorites"] = {
+},
+["showExtraCombatToggles"] = false,
+["__fmDebugCombatText"] = false,
+["incomingOverrides"] = {
+},
+["floatingTextGravity"] = 0.5,
+["selectedFont"] = "Popular/Roboto-Bold.ttf",
+}
---
+++ 2026-04-29_15-56-43/RaiderIO.lua
@@ -0,0 +1,64 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Arkllion-kazzak"] = true,
+["eu-Chadopan-dragonmaw"] = true,
+["eu-Enridudu-draenor"] = true,
+["eu-Tkialohunter-kazzak"] = true,
+["eu-Belsnus-twisting-nether"] = true,
+["eu-Macdk-antonidas"] = true,
+["eu-Barenen-argent-dawn"] = true,
+["eu-Randybonks-draenor"] = true,
+["eu-Abräkadabra-ragnaros"] = true,
+["eu-Adelune-kazzak"] = true,
+["eu-Sorrycapss-twisting-nether"] = true,
+["eu-Oakdem-stormscale"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Kfcmanagerx-hyjal"] = true,
+["eu-Hatethisspec-twisting-nether"] = true,
+["eu-Grímr-drakthul"] = true,
+["eu-Notalok-ravencrest"] = true,
+["eu-Jurko-kazzak"] = true,
+["eu-Nistra-drakthul"] = true,
+["eu-Devnug-silvermoon"] = true,
+["eu-Alud-ravencrest"] = true,
+["eu-Arthanian-doomhammer"] = true,
+["eu-Palytra-silvermoon"] = true,
+["eu-Namtia-silvermoon"] = true,
+["eu-Cinnder-silvermoon"] = true,
+["eu-party4-draenor"] = true,
+["eu-Çømgøbbler-tarren-mill"] = true,
+["eu-Hexachain-kazzak"] = true,
+["eu-Krizzx-silvermoon"] = true,
+["eu-Lastwisp-silvermoon"] = true,
+["eu-Serylyn-sanguino"] = true,
+["eu-Darkrangers-stormscale"] = true,
+["eu-Crispiepräst-stormscale"] = true,
+["eu-Sarkian-draenor"] = true,
+["eu-Ryoshin-ravencrest"] = true,
+["eu-Pingers-silvermoon"] = true,
+["eu-Idonteventry-silvermoon"] = true,
+["eu-Reaperspride-kazzak"] = true,
+["eu-Biraa-tarren-mill"] = true,
+["eu-Adhonis-burning-blade"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
