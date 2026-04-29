5f0a3d670f8abd4a8869900885a0276ab6f65618
fborch <fborch@gmail.com>
2026-04-29 11:46:53 +0200

    Auto-backup WoW settings: 2026-04-29_11-46-52

---
+++ 2026-04-29_11-46-52/RaiderIO.lua
@@ -0,0 +1,25 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Coorlan-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Coorlan-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
---
+++ 2026-04-29_11-46-52/XIV_Databar_Continued.lua
@@ -0,0 +1,487 @@
+
+XIVBarDB = {
+["char"] = {
+["Decoran - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Nothree - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Corzumo - Stormrage"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Abaray - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Unlockit - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Corry - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Cooriam - Draenor"] = {
+["portItem"] = {
+["portId"] = 110560,
+},
+},
+["Altoki - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Coorios - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Gembank - Stormrage"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corwing - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Coorlan - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corzos - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corsell - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corra - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Decor - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+["Coriiam - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Faile - Stormrage"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Sedore - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corzumo - Draenor"] = {
+["portItem"] = {
+["portId"] = 140192,
+["text"] = "Dalaran Hearthstone",
+},
+},
+["Corpy - Draenor"] = {
+["portItem"] = {
+["text"] = "Dalaran Hearthstone",
+["portId"] = 140192,
+},
+},
+},
+["profileKeys"] = {
+["Decoran - Draenor"] = "Default",
+["Nothree - Draenor"] = "Default",
+["Corzumo - Stormrage"] = "Default",
+["Abaray - Draenor"] = "Default",
+["Unlockit - Draenor"] = "Default",
+["Corry - Draenor"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Altoki - Draenor"] = "Default",
+["Coorios - Draenor"] = "Default",
+["Gembank - Stormrage"] = "Default",
+["Corwing - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+["Corsell - Draenor"] = "Default",
+["Corra - Draenor"] = "Default",
+["Decor - Draenor"] = "Default",
+["Coriiam - Draenor"] = "Default",
+["Faile - Stormrage"] = "Default",
+["Sedore - Draenor"] = "Default",
+["Corzumo - Draenor"] = "Default",
+["Corpy - Draenor"] = "Default",
+},
+["global"] = {
+["characters"] = {
+["Corzumo-Stormrage"] = {
+["dailyMoney"] = 0,
+["sessionMoney"] = 0,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260424,
+["class"] = "ROGUE",
+["currentMoney"] = 6221563,
+["realm"] = "Stormrage",
+},
+["Coriiam-Draenor"] = {
+["dailyMoney"] = 400,
+["faction"] = "Alliance",
+["sessionMoney"] = 400,
+["currentMoney"] = 10400,
+["class"] = "DEMONHUNTER",
+["lastLoginDate"] = 20260327,
+["realm"] = "Draenor",
+},
+["Corry-Draenor"] = {
+["dailyMoney"] = 0,
+["faction"] = "Horde",
+["sessionMoney"] = 0,
+["currentMoney"] = 292251758,
+["class"] = "ROGUE",
+["lastLoginDate"] = 20260420,
+["realm"] = "Draenor",
+},
+["Corpy-Draenor"] = {
+["dailyMoney"] = 0,
+["sessionMoney"] = 0,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260409,
+["class"] = "MAGE",
+["currentMoney"] = 10000,
+["realm"] = "Draenor",
+},
+["Corwing-Draenor"] = {
+["dailyMoney"] = 44505,
+["faction"] = "Horde",
+["sessionMoney"] = 44505,
+["currentMoney"] = 44505,
+["class"] = "EVOKER",
+["lastLoginDate"] = 20260417,
+["realm"] = "Draenor",
+},
+["Faile-Stormrage"] = {
+["dailyMoney"] = 0,
+["faction"] = "Alliance",
+["sessionMoney"] = 0,
+["currentMoney"] = 91487297,
+["class"] = "PALADIN",
+["lastLoginDate"] = 20260415,
+["realm"] = "Stormrage",
+},
+["Coorios-Draenor"] = {
+["dailyMoney"] = 0,
+["faction"] = "Alliance",
+["sessionMoney"] = 0,
+["currentMoney"] = 10000,
+["class"] = "DRUID",
+["lastLoginDate"] = 20260323,
+["realm"] = "Draenor",
+},
+["Decor-Draenor"] = {
+["dailyMoney"] = 0,
+["sessionMoney"] = 0,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260415,
+["class"] = "WARLOCK",
+["currentMoney"] = 3372268,
+["realm"] = "Draenor",
+},
+["Cooriam-Draenor"] = {
+["dailyMoney"] = 165945119,
+["sessionMoney"] = 190640184,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260429,
+["class"] = "MONK",
+["currentMoney"] = 2148600328,
+["realm"] = "Draenor",
+},
+["Altoki-Draenor"] = {
+["dailyMoney"] = 0,
+["sessionMoney"] = 0,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260415,
+["class"] = "SHAMAN",
+["currentMoney"] = 704661257,
+["realm"] = "Draenor",
+},
+["Corsell-Draenor"] = {
+["dailyMoney"] = -290000000,
+["faction"] = "Horde",
+["sessionMoney"] = -290000000,
+["currentMoney"] = 3490737,
+["class"] = "ROGUE",
+["lastLoginDate"] = 20260415,
+["realm"] = "Draenor",
+},
+["Unlockit-Draenor"] = {
+["dailyMoney"] = 0,
+["sessionMoney"] = 0,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260420,
+["class"] = "DRUID",
+["currentMoney"] = 0,
+["realm"] = "Draenor",
+},
+["Nothree-Draenor"] = {
+["dailyMoney"] = 719,
+["sessionMoney"] = 719,
+["faction"] = "Alliance",
+["lastLoginDate"] = 19990000,
+["class"] = "ROGUE",
+["currentMoney"] = 719,
+["realm"] = "Draenor",
+},
+["Decoran-Draenor"] = {
+["dailyMoney"] = 275682,
+["faction"] = "Horde",
+["sessionMoney"] = 275682,
+["currentMoney"] = 16377270,
+["class"] = "WARLOCK",
+["lastLoginDate"] = 20260424,
+["realm"] = "Draenor",
+},
+["Coorlan-Draenor"] = {
+["dailyMoney"] = -1816770,
+["faction"] = "Horde",
+["sessionMoney"] = 14158692,
+["currentMoney"] = 781619773,
+["class"] = "WARRIOR",
+["lastLoginDate"] = 20260429,
+["realm"] = "Draenor",
+},
+["Gembank-Stormrage"] = {
+["dailyMoney"] = 0,
+["faction"] = "Horde",
+["sessionMoney"] = 0,
+["currentMoney"] = 5591497,
+["class"] = "MAGE",
+["lastLoginDate"] = 20260415,
+["realm"] = "Stormrage",
+},
+["Abaray-Draenor"] = {
+["dailyMoney"] = 66849073,
+["sessionMoney"] = 66507473,
+["faction"] = "Horde",
+["lastLoginDate"] = 20260428,
+["class"] = "DRUID",
+["currentMoney"] = 84873834,
+["realm"] = "Draenor",
+},
+["Corra-Draenor"] = {
+["dailyMoney"] = -3085163,
+["faction"] = "Alliance",
+["sessionMoney"] = -3085163,
+["currentMoney"] = 114450895,
+["class"] = "HUNTER",
+["lastLoginDate"] = 20260428,
+["realm"] = "Draenor",
+},
+["Sedore-Draenor"] = {
+["dailyMoney"] = 19000000,
+["faction"] = "Horde",
+["sessionMoney"] = 19000000,
+["currentMoney"] = 1342165044,
+["class"] = "PRIEST",
+["lastLoginDate"] = 20260428,
+["realm"] = "Draenor",
+},
+["Corzumo-Draenor"] = {
+["dailyMoney"] = 10001420,
+["faction"] = "Horde",
+["sessionMoney"] = 10001420,
+["currentMoney"] = 10001420,
+["class"] = "ROGUE",
+["lastLoginDate"] = 20260421,
+["realm"] = "Draenor",
+},
+["Corzos-Draenor"] = {
+["dailyMoney"] = 15417318,
+["faction"] = "Horde",
+["sessionMoney"] = 2032188,
+["currentMoney"] = 1906937096,
+["class"] = "ROGUE",
+["lastLoginDate"] = 20260428,
+["realm"] = "Draenor",
+},
+},
+},
+["profiles"] = {
+["Default"] = {
+["selectedHouseGuid"] = "Opaque-1",
+["selectedHearthstones"] = {
+},
+["general"] = {
+["enableFreePlacement"] = true,
+["yOffset"] = -526,
+["barFullscreen"] = false,
+["moduleSpacing"] = 26,
+["freePlacementInitialized"] = true,
+["modulePlacements"] = {
+["tradeskill"] = {
+["anchorPoint"] = "CENTER",
+["x"] = -607,
+["initialX"] = 147.666748046875,
+["initialAnchorPoint"] = "CENTER",
+["captured"] = true,
+},
+["talent"] = {
+["anchorPoint"] = "CENTER",
+["x"] = -848,
+["initialX"] = -109.2498779296875,
+["initialAnchorPoint"] = "CENTER",
+["captured"] = true,
+},
+["reputation"] = {
+["anchorPoint"] = "LEFT",
+["x"] = 1,
+["initialX"] = 1,
+["initialAnchorPoint"] = "LEFT",
+["captured"] = true,
+},
+["travel"] = {
+["anchorPoint"] = "RIGHT",
+["x"] = -124,
+["initialX"] = -1,
+["initialAnchorPoint"] = "RIGHT",
+["captured"] = true,
+},
+["microMenu"] = {
+["anchorPoint"] = "LEFT",
+["x"] = 1,
+["initialX"] = 1,
+["initialAnchorPoint"] = "LEFT",
+["captured"] = true,
+},
+["vault"] = {
+["anchorPoint"] = "RIGHT",
+["x"] = -1,
+["initialX"] = -1,
+["initialAnchorPoint"] = "RIGHT",
+["captured"] = true,
+},
+["currency"] = {
+["anchorPoint"] = "CENTER",
+["x"] = 316.5836181640625,
+["initialX"] = 315.583251953125,
+["initialAnchorPoint"] = "CENTER",
+["captured"] = true,
+},
+["gold"] = {
+["anchorPoint"] = "RIGHT",
+["x"] = -1119,
+["initialX"] = -295,
+["initialAnchorPoint"] = "RIGHT",
+["captured"] = true,
+},
+["clock"] = {
+["anchorPoint"] = "CENTER",
+["x"] = 0,
+["initialX"] = 0,
+["initialAnchorPoint"] = "CENTER",
+["captured"] = true,
+},
+["MasterVolume"] = {
+["anchorPoint"] = "LEFT",
+["x"] = 1,
+["initialX"] = 1,
+["initialAnchorPoint"] = "LEFT",
+["captured"] = true,
+},
+["armor"] = {
+["anchorPoint"] = "LEFT",
+["x"] = 3,
+["initialX"] = 0,
+["initialAnchorPoint"] = "LEFT",
+["captured"] = false,
+},
+["system"] = {
+["anchorPoint"] = "CENTER",
+["x"] = 279,
+["initialX"] = 406.1666259765625,
+["initialAnchorPoint"] = "CENTER",
+["captured"] = true,
+},
+},
+["barPadding"] = 0,
+},
+["color"] = {
+["barColor"] = {
+["r"] = 0,
+["g"] = 0.08627451211214066,
+["b"] = 0.05098039656877518,
+},
+},
+["hideAdditionalTooltipText"] = true,
+["hearthstonesList"] = {
+[180290] = "Night Fae Hearthstone",
+[208704] = "Deepdweller's Earthen Hearthstone",
+[172179] = "Eternal Traveler's Hearthstone",
+[209035] = "Hearthstone of the Flame",
+},
+["modules"] = {
+["talent"] = {
+["loadoutSwitcherEnabled"] = false,
+},
+["microMenu"] = {
+["spellMergedIntoTalent"] = true,
+["enabled"] = false,
+},
+["armor"] = {
+["enabled"] = false,
+},
+},
+["hideHearthstoneButton"] = false,
+["hearthstoneCache"] = {
+[208704] = "Deepdweller's Earthen Hearthstone",
+[172179] = "Eternal Traveler's Hearthstone",
+[180290] = "Night Fae Hearthstone",
+[6948] = "Hearthstone",
+},
+["enableMythicPortals"] = true,
+["text"] = {
+["flags"] = 2,
+["fontSize"] = 15,
+["font"] = "quazii",
+},
+["hidePortButton"] = false,
+},
+},
+}
