cd1575d3f99ce6621431bab79677ef71f0483ab1
fborch <fborch@gmail.com>
2026-04-29 12:19:13 +0200

    Auto-backup WoW settings: 2026-04-29_12-19-13

---
+++ 2026-04-29_12-19-13/RaiderIO.lua
@@ -0,0 +1,64 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Cooriam-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Zukehiro-draenor"] = true,
+["eu-Uglewedel-draenor"] = true,
+["eu-Rcf-draenor"] = true,
+["eu-Trafysett-draenor"] = true,
+["eu-Glumpz-draenor"] = true,
+["eu-Yosimitsu-draenor"] = true,
+["eu-Badoodledk-draenor"] = true,
+["eu-Fatyez-antonidas"] = true,
+["eu-Mowd-draenor"] = true,
+["eu-Látombm-draenor"] = true,
+["eu-Jaselle-blackrock"] = true,
+["eu-Excalliber-draenor"] = true,
+["eu-Rikiman-draenor"] = true,
+["eu-Mihoka-draenor"] = true,
+["eu-Hirobee-draenor"] = true,
+["eu-Orvîz-draenor"] = true,
+["eu-Talktomyshiv-draenor"] = true,
+["eu-Retutem-draenor"] = true,
+["eu-Talism-draenor"] = true,
+["eu-Skimiz-draenor"] = true,
+["eu-Mondex-draenor"] = true,
+["eu-Hefestas-draenor"] = true,
+["eu-Xaviola-draenor"] = true,
+["eu-Bloodsicon-draenor"] = true,
+["eu-Ryzlock-draenor"] = true,
+["eu-Twinm-draenor"] = true,
+["eu-Iceflamerino-draenor"] = true,
+["eu-Thyrrina-draenor"] = true,
+["eu-Healgearl-draenor"] = true,
+["eu-Pappsvärd-draenor"] = true,
+["eu-Laroq-draenor"] = true,
+["eu-Teapack-draenor"] = true,
+["eu-Holyfarter-draenor"] = true,
+["eu-Temerax-draenor"] = true,
+["eu-Brusertwo-draenor"] = true,
+["eu-Megachef-draenor"] = true,
+["eu-Lufì-draenor"] = true,
+["eu-Battlecaster-draenor"] = true,
+["eu-Cizmar-draenor"] = true,
+["eu-Koorl-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
