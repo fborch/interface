54c2a5e4c042346ee894d8b5a957a5199e5d4269
fborch <fborch@gmail.com>
2026-04-29 12:21:18 +0200

    Auto-backup WoW settings: 2026-04-29_12-21-18

---
+++ 2026-04-29_12-21-18/FontMagic.lua
@@ -0,0 +1,20 @@
+
+FontMagicDB = {
+["minimapAngle"] = 225,
+["minimapHide"] = false,
+["combatOverrides"] = {
+},
+["selectedFont"] = "Popular/Roboto-Bold.ttf",
+["extraCombatOverrides"] = {
+},
+["combatTextOutlineMode"] = "NONE",
+["__fmPopularFolderMigrationDone"] = true,
+["incomingOverrides"] = {
+},
+["favorites"] = {
+},
+["__fmDebugCombatText"] = false,
+["showExtraCombatToggles"] = false,
+["floatingTextGravity"] = 0.5,
+["floatingTextFadeDuration"] = 1,
+}
