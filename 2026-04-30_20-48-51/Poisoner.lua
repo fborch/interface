30b61def910a5ff3341fbe0c783cadedf3640e0c
fborch <fborch@gmail.com>
2026-04-30 20:48:52 +0200

    Auto-backup WoW settings: 2026-04-30_20-48-51

---
+++ 2026-04-30_20-48-51/Poisoner.lua
@@ -0,0 +1,30 @@
+
+PoisonerDB = {
+["namespaces"] = {
+},
+["profileKeys"] = {
+["Sedore - Draenor"] = "Default",
+["Corra - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Abaray - Draenor"] = "Default",
+},
+["profiles"] = {
+["Default"] = {
+["poisons"] = {
+["lastused"] = {
+["Lethal"] = 2823,
+["Non-Lethal"] = 3408,
+},
+},
+["freebutton"] = {
+["point"] = {
+["y"] = 27.00015620624754,
+["rp"] = "BOTTOMLEFT",
+["x"] = 2334.000456091948,
+},
+},
+},
+},
+}
