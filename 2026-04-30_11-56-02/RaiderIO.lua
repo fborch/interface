a80f593d93dc75a1c986b4715644c38762d7f286
fborch <fborch@gmail.com>
2026-04-30 11:56:03 +0200

    Auto-backup WoW settings: 2026-04-30_11-56-02

---
+++ 2026-04-30_11-56-02/RaiderIO.lua
@@ -0,0 +1,43 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Onidh-kazzak"] = true,
+["eu-Luqt-argent-dawn"] = true,
+["eu-Sleyrot-draenor"] = true,
+["eu-Pokoriel-draenor"] = true,
+["eu-Ely-auchindoun"] = true,
+["eu-Greepo-thrall"] = true,
+["eu-Lazzan-draenor"] = true,
+["eu-Evilfhänä-draenor"] = true,
+["eu-Scourgespam-antonidas"] = true,
+["eu-Neoncow-laughing-skull"] = true,
+["eu-Ourshag-draenor"] = true,
+["eu-Gashimonk-stormscale"] = true,
+["eu-Deathshand-antonidas"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Magetopan-tarren-mill"] = true,
+["eu-Blis-blackmoore"] = true,
+["eu-Cotillárd-silvermoon"] = true,
+["eu-Kurtibrew-blackhand"] = true,
+["eu-Arapa-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
---
+++ 2026-04-30_11-56-02/WorldQuestTab.lua
@@ -0,0 +1,83 @@
+
+BWQDB = {
+["char"] = {
+["Altoki - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Decoran - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Nothree - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Coorlan - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Faile - Stormrage"] = {
+["activeProfile"] = 0,
+},
+["Abaray - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzos - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzumo - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corra - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Unlockit - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corry - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Corzumo - Stormrage"] = {
+["activeProfile"] = 0,
+},
+["Cooriam - Draenor"] = {
+["activeProfile"] = 0,
+},
+["Sedore - Draenor"] = {
+["activeProfile"] = 0,
+},
+},
+["global"] = {
+["versionCheck"] = 120011,
+["profiles"] = {
+[0] = {
+["name"] = "Default",
+["colors"] = {
+},
+["list"] = {
+},
+["external"] = {
+},
+["pin"] = {
+},
+["general"] = {
+},
+["filters"] = {
+},
+},
+},
+},
+["profileKeys"] = {
+["Altoki - Draenor"] = "Default",
+["Decoran - Draenor"] = "Default",
+["Nothree - Draenor"] = "Default",
+["Coorlan - Draenor"] = "Default",
+["Faile - Stormrage"] = "Default",
+["Abaray - Draenor"] = "Default",
+["Corzos - Draenor"] = "Default",
+["Corzumo - Draenor"] = "Default",
+["Corra - Draenor"] = "Default",
+["Unlockit - Draenor"] = "Default",
+["Corry - Draenor"] = "Default",
+["Corzumo - Stormrage"] = "Default",
+["Cooriam - Draenor"] = "Default",
+["Sedore - Draenor"] = "Default",
+},
+}
