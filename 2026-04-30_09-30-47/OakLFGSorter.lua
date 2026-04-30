9b371cc0f04a92b0a592251991e25c8aea8462f4
fborch <fborch@gmail.com>
2026-04-30 09:30:48 +0200

    Auto-backup WoW settings: 2026-04-30_09-30-47

---
+++ 2026-04-30_09-30-47/OakLFGSorter.lua
@@ -0,0 +1,49 @@
+
+OakLFGSorterDB = {
+["windowHeight"] = 444,
+["searchPersistSignupNote"] = false,
+["themePreset"] = "OAK_TEAL",
+["scale"] = 1,
+["autoOpenSearch"] = true,
+["muteApplicantPing"] = true,
+["autoOpen"] = true,
+["frameUserPlaced"] = true,
+["frameSize"] = {
+1099.999877929688,
+735.7501220703125,
+},
+["showPartyKeys"] = true,
+["fontSize"] = 14,
+["theme"] = "classic",
+["browserCategoryOverrides"] = {
+},
+["searchQuickSignupRoles"] = {
+["DAMAGER"] = false,
+["TANK"] = false,
+["HEALER"] = true,
+},
+["framePos"] = {
+"BOTTOMLEFT",
+"BOTTOMLEFT",
+214.9165954589844,
+251.9997406005859,
+},
+["showRegionFlags"] = false,
+["searchQuickSignup"] = true,
+["regionFilters"] = {
+},
+["showRegions"] = false,
+["hideNotes"] = false,
+["autoHideFilledRoles"] = false,
+["browserFilters"] = {
+},
+["fontName"] = "quazii",
+["windowWidth"] = 1100,
+["browserCategoryKey"] = "DUNGEONS",
+["windowOpacity"] = 0.85,
+["themeStyle"] = "MODERN_CLEAN",
+["minimapButton"] = {
+["angle"] = 220,
+},
+["hideMinimapButton"] = false,
+}
---
+++ 2026-04-30_09-30-47/RaiderIO.lua
@@ -0,0 +1,33 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Corzos-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Aftermathy-bronze-dragonflight"] = true,
+["eu-Sìxseven-draenor"] = true,
+["eu-Dvìant-draenor"] = true,
+["eu-Purplereígn-draenor"] = true,
+["eu-Corzos-draenor"] = true,
+["eu-Aeshy-draenor"] = true,
+["eu-Icanseeyou-draenor"] = true,
+["eu-Snorelaxx-draenor"] = true,
+["eu-Jezuscriits-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
