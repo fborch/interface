be72980c58a7f6a0af31bf63984f694cbd51d8dc
fborch <fborch@gmail.com>
2026-05-01 11:42:19 +0200

    Auto-backup WoW settings: 2026-05-01_11-42-18

---
+++ 2026-05-01_11-42-18/RaiderIO.lua
@@ -0,0 +1,36 @@
+
+RaiderIO_Config = {
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["hide"] = false,
+["lock"] = false,
+},
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+}
+RaiderIO_LastCharacter = "eu-Cooriam-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Quelyra-draenor"] = true,
+["eu-Sfcdendra-draenor"] = true,
+["eu-Danarr-draenor"] = true,
+["eu-Porph-draenor"] = true,
+["eu-Radflame-draenor"] = true,
+["eu-Oreganocow-draenor"] = true,
+["eu-Theodosus-draenor"] = true,
+["eu-Flyte-draenor"] = true,
+["eu-Azpire-draenor"] = true,
+["eu-Antar-draenor"] = true,
+["eu-Swipels-draenor"] = true,
+["eu-Frostguden-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
---
+++ 2026-05-01_11-42-18/SexyMap.lua
@@ -0,0 +1,132 @@
+
+SexyMap2DB = {
+["presets"] = {
+},
+["Cooriam-Draenor"] = {
+["zonetext"] = {
+["outline"] = "NONE",
+["fontColor"] = {
+},
+["xOffset"] = 0,
+["monochrome"] = false,
+["yOffset"] = 0,
+["bgColor"] = {
+["a"] = 1,
+["b"] = 0,
+["g"] = 0,
+["r"] = 0,
+},
+["font"] = "Friz Quadrata TT",
+["useSecureButton"] = false,
+["borderColor"] = {
+["a"] = 1,
+["b"] = 0,
+["g"] = 0,
+["r"] = 0,
+},
+},
+["coordinates"] = {
+["enabled"] = true,
+["fontColor"] = {
+},
+["xOffset"] = 0,
+["monochrome"] = false,
+["updateRate"] = 1,
+["yOffset"] = 10,
+["font"] = "Friz Quadrata TT",
+["borderColor"] = {
+},
+["coordPrecision"] = "%d,%d",
+["backgroundColor"] = {
+},
+["outline"] = "NONE",
+},
+["buttons"] = {
+["radius"] = 10,
+["lockDragging"] = false,
+["allowDragging"] = true,
+["scale"] = 1,
+["dragPositions"] = {
+},
+["visibilitySettings"] = {
+["MinimapZoomIn"] = "never",
+["MiniMapMailFrame"] = "always",
+["MiniMapWorldMapButton"] = "never",
+["MinimapZoomOut"] = "never",
+["AddonCompartmentFrame"] = "never",
+["GarrisonLandingPageMinimapButton"] = "always",
+["CraftingOrder"] = "always",
+["TimeManagerClockButton"] = "always",
+["SexyMapZoneTextButton"] = "always",
+},
+["controlVisibility"] = true,
+},
+["clock"] = {
+["bgColor"] = {
+["a"] = 1,
+["b"] = 0,
+["g"] = 0,
+["r"] = 0,
+},
+["font"] = "Friz Quadrata TT",
+["fontColor"] = {
+},
+["borderColor"] = {
+["a"] = 1,
+["b"] = 0,
+["g"] = 0,
+["r"] = 0,
+},
+["xOffset"] = 0,
+["monochrome"] = false,
+["outline"] = "NONE",
+["yOffset"] = 0,
+},
+["borders"] = {
+["applyPreset"] = false,
+["borders"] = {
+},
+["backdrop"] = {
+["show"] = true,
+["textureColor"] = {
+["r"] = 0,
+["g"] = 0,
+["b"] = 0,
+},
+["settings"] = {
+["bgFile"] = 137056,
+["edgeFile"] = 137057,
+["edgeSize"] = 13,
+["tile"] = false,
+["insets"] = {
+["top"] = 4,
+["right"] = 4,
+["left"] = 4,
+["bottom"] = 4,
+},
+},
+["borderColor"] = {
+},
+["scale"] = 1.07,
+},
+["hideBlizzard"] = true,
+},
+["movers"] = {
+["lockTopWidget"] = false,
+["moveTopWidget"] = false,
+["lockCaptureBar"] = false,
+["moveCaptureBar"] = false,
+["moverPositions"] = {
+},
+},
+["core"] = {
+["clamp"] = true,
+["northTag"] = true,
+["lock"] = true,
+["scale"] = 1.43,
+["shape"] = 130871,
+["autoZoom"] = 5,
+["rightClickToConfig"] = true,
+},
+},
+}
