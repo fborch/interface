d74cccfd979080b7a02d8b246c93440160027f35
fborch <fborch@gmail.com>
2026-05-01 13:07:00 +0200

    Auto-backup WoW settings: 2026-05-01_13-06-59

---
+++ 2026-05-01_13-06-59/RaiderIO.lua
@@ -0,0 +1,67 @@
+
+RaiderIO_Config = {
+["profilePoint"] = {
+["y"] = 0,
+["x"] = -16.00000190734863,
+["point"] = "TOPLEFT",
+},
+["minimapIcon"] = {
+["minimapPos"] = 180,
+["showInCompartment"] = true,
+["lock"] = false,
+["hide"] = false,
+},
+}
+RaiderIO_LastCharacter = "eu-Abaray-draenor"
+RaiderIO_MissingCharacters = {
+["eu-Eoleena-kazzak"] = true,
+["eu-Svanah-blackrock"] = true,
+["eu-Herreløs-draenor"] = true,
+["eu-Tadpowl-draenor"] = true,
+["eu-Reiskuli-frostwhisper"] = true,
+["eu-Brutariz-turalyon"] = true,
+["eu-Mägée-draenor"] = true,
+["eu-Pivozavrishe-silvermoon"] = true,
+["eu-Tracydwagon-blackrock"] = true,
+["eu-Lickaflame-ragnaros"] = true,
+["eu-z-argent-dawn"] = true,
+["eu-Abaray-draenor"] = true,
+["eu-Celínedijon-tarren-mill"] = true,
+["eu-Jessabélla-draenor"] = true,
+["eu-Пятёрочкка-howling-fjord"] = true,
+["eu-Remidas-zuluhed"] = true,
+["eu-Spyröôts-hyjal"] = true,
+["eu-Simpette-turalyon"] = true,
+["eu-Ysros-blackmoore"] = true,
+["eu-Noxasbrew-blackrock"] = true,
+["eu-Diyala-blackrock"] = true,
+["eu-Hartasz-die-aldor"] = true,
+["eu-Intofire-draenor"] = true,
+["eu-Skazune-archimonde"] = true,
+["eu-Costersyfi-draenor"] = true,
+["eu-Socialfailur-ravencrest"] = true,
+["eu-Sunskins-draenor"] = true,
+["eu-Insanydh-drakthul"] = true,
+["eu-Dravak-frostmane"] = true,
+["eu-Freei-doomhammer"] = true,
+["eu-Harani-quelthalas"] = true,
+["eu-Myouren-norgannon"] = true,
+["eu-Ginti-sporeggar"] = true,
+["eu-Gotnerfed-draenor"] = true,
+["eu-Elìo-antonidas"] = true,
+["eu-Eruiza-draenor"] = true,
+["eu-Gronkowsky-ragnaros"] = true,
+["eu-Shinan-draenor"] = true,
+["eu-Xyóz-argent-dawn"] = true,
+["eu-Korzoon-draenor"] = true,
+["eu-Sacerblast-blackhand"] = true,
+["eu-Oririlre-blackmoore"] = true,
+["eu-Atene-draenor"] = true,
+}
+RaiderIO_MissingServers = {
+}
+RaiderIO_CachedRuns = nil
+RaiderIO_RWF = {
+}
+RaiderIO_CompletedReplays = {
+}
