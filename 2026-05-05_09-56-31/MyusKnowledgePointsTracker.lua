c0708d82b32c2309dc32bf34799d69f0d480b8b5
fborch <fborch@gmail.com>
2026-05-05 09:56:32 +0200

    Auto-backup WoW settings: 2026-05-05_09-56-31

---
+++ 2026-05-05_09-56-31/MyusKnowledgePointsTracker.lua
@@ -0,0 +1,28 @@
+
+MKPT_Config = {
+["char"] = {
+["Cooriam - Draenor"] = {
+["state"] = {
+["show"] = false,
+},
+},
+},
+["profileKeys"] = {
+["Altoki - Draenor"] = "Altoki - Draenor",
+["Sedore - Draenor"] = "Sedore - Draenor",
+["Corzos - Draenor"] = "Corzos - Draenor",
+["Faile - Stormrage"] = "Faile - Stormrage",
+["Corwing - Draenor"] = "Corwing - Draenor",
+["Coorlan - Draenor"] = "Coorlan - Draenor",
+["Nothree - Draenor"] = "Nothree - Draenor",
+["Abaray - Draenor"] = "Abaray - Draenor",
+["Corzumo - Stormrage"] = "Corzumo - Stormrage",
+["Decoran - Draenor"] = "Decoran - Draenor",
+["Corra - Draenor"] = "Corra - Draenor",
+["Unlockit - Draenor"] = "Unlockit - Draenor",
+["Corry - Draenor"] = "Corry - Draenor",
+["Corzumo - Draenor"] = "Corzumo - Draenor",
+["Cooriam - Draenor"] = "Cooriam - Draenor",
+["Gembank - Stormrage"] = "Gembank - Stormrage",
+},
+}
